a7030966f6df1cc257b83fe3d5afaf75f87a3a30
Nutchayaporn <54405491+Nutchayaporn@users.noreply.github.com>
2022-05-10 17:42:15 +0700

    Feature/Frontend

--- back-end/index.js
+++ back-end/index.js
@@ -7,6 +7,26 @@ const phone = require("./model/phoneNum")
 const app = express();
 const port = 4000;
 
+
+const { PrometheusExporter } = require('@opentelemetry/exporter-prometheus');
+const { MeterProvider }  = require('@opentelemetry/sdk-metrics-base');
+
+// Add your port and startServer to the Prometheus options
+const options = {port: 9464, startServer: true};
+const exporter = new PrometheusExporter(options);
+
+// Register the exporter
+const meter = new MeterProvider({
+  exporter,
+  interval: 1000,
+}).getMeter('demo-prometheus');
+
+// Now, start recording data
+const counter = meter.createCounter('count_database', {
+  description: 'count database'
+});
+
+
 mongoose.Promise = global.Promise 
 mongoose.connect(configs.mongouri, {
     useNewUrlParser : true,
@@ -28,7 +48,10 @@ app.get('/test', async(req, res) => {
 
 app.get('/user', async(req, res) => {
     try {
-       
+        counter.add(1, { service_name: "myservice" });
+        counter.add(1, { service_name: "myservice", status: "success", path: "/user" });
+        counter.add(1, { service_name: "myservice", status: "failure", path: "/user" });
+        counter.add(1, { service_name: "myservice", status: "data not found", path: "/user" });
         // let a = new UserModel({ title : "DED" })
         // await a.save()
         const result = await UserModel.find()
